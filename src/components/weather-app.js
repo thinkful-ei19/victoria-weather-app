41c3a4b09231774468c5042f112b5d9a84c05a87
Victoria Daug <victoria.daug@gmail.com>
2018-04-12 17:46:44 +0300

    refactoring data

--- src/components/weather-app.js
+++ src/components/weather-app.js
@@ -5,83 +5,34 @@ import SearchForm from './search-form';
 import WeatherImage from './weather-image';
 import WeatherTemp from './weather-temp';
 
+import Data from '../data';
+
 class WeatherApp extends Component {
   constructor(props) {
     super(props);
 
       this.state = {
-        weatherRequest: 'monday',
-        days: [
-          {
-              name: 'monday',
-              dayTemp: '25°C',
-              nightTemp: '15°C',
-              forecast: 'Cloudy'
-
-          },
-          {
-              name: 'tuesday',
-              dayTemp: '31°C',
-              nightTemp:'19°C',
-              forecast: 'Sunny'
-
-          },
-          {
-              name: 'wednesday',
-              dayTemp: '23°C',
-              nightTemp: '15°C',
-              forecast: 'Cloudy'
-
-          },
-          {
-              name: 'thursday',
-              dayTemp: '30°C',
-              nightTemp: '17°C',
-              forecast: 'Sunny'
-
-          },
-          {
-              name: 'friday',
-              dayTemp: '28°C',
-              nightTemp: '16°C',
-              forecast: 'Sunny'
-
-          },
-          {
-              name: 'saturday',
-              dayTemp: '25°C',
-              nightTemp: '14°C',
-              forecast: 'Rainy'
-
-          },
-          {
-              name: 'sunday',
-              dayTemp: '-2°C',
-              nightTemp: '-10°C',
-              forecast: 'Snowy'
-
-          }
-        ]
+        weather: Data[0]
       }
   }
 
   weatherRequest(searchTerm) {
     this.setState({
-      weatherRequest: searchTerm.toLowerCase()
+      weather: Data.find(day => {
+          return day.name.includes(searchTerm.toLowerCase())
+        })
     })
   }
 
   render (){
+    
+    const {weather} = this.state
 
-      const selected = this.state.days.find(day => {
-          return day.name.includes(this.state.weatherRequest)
-        });
-console.log(selected.forecast)
     return (
       <div>
-        <SearchForm day={this.state.days} onClick={searchTerm => this.weatherRequest(searchTerm)}/>
-        <WeatherImage forecast={selected.forecast} name={selected.name}/>
-        <WeatherTemp dayTemp={selected.dayTemp} nightTemp={selected.nightTemp}/>
+        <SearchForm onClick={searchTerm => this.weatherRequest(searchTerm)}/>
+        <WeatherImage forecast={weather.forecast} name={weather.name}/>
+        <WeatherTemp dayTemp={weather.dayTemp} nightTemp={weather.nightTemp}/>
       </div>
     );
   }
---
+++ src/data.js
@@ -0,0 +1,51 @@
+export default [
+  {
+      name: 'monday',
+      dayTemp: '25°C',
+      nightTemp: '15°C',
+      forecast: 'Cloudy'
+
+  },
+  {
+      name: 'tuesday',
+      dayTemp: '31°C',
+      nightTemp:'19°C',
+      forecast: 'Sunny'
+
+  },
+  {
+      name: 'wednesday',
+      dayTemp: '23°C',
+      nightTemp: '15°C',
+      forecast: 'Cloudy'
+
+  },
+  {
+      name: 'thursday',
+      dayTemp: '30°C',
+      nightTemp: '17°C',
+      forecast: 'Sunny'
+
+  },
+  {
+      name: 'friday',
+      dayTemp: '28°C',
+      nightTemp: '16°C',
+      forecast: 'Sunny'
+
+  },
+  {
+      name: 'saturday',
+      dayTemp: '25°C',
+      nightTemp: '14°C',
+      forecast: 'Rainy'
+
+  },
+  {
+      name: 'sunday',
+      dayTemp: '-2°C',
+      nightTemp: '-10°C',
+      forecast: 'Snowy'
+
+  }
+]
